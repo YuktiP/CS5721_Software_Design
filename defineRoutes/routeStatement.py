df4dc4a33b5da4b041427079423b24aaaca9dff1
Ciara <ciaratully0@gmail.com>
2020-12-10 16:50:08 +0000

    View Statement route for customer.

---
+++ defineRoutes/routeStatement.py
@@ -0,0 +1,28 @@
+import datetime
+from app import app, db, current_user
+from flask import render_template, session
+from models.UserAuthentication import User
+from models.Statement import Statement
+from app import current_user, login_manager
+
+
+
+
+@app.route('/ViewStatement', methods = ['GET'])
+def statement(): 
+    print(current_user)
+    userId=current_user.get_id()
+    statementObj = Statement("userId")
+    fetchedStatement = statementObj.getStatement()
+    return(render_template("ViewStatement.html", statement=statementObj))
+    
+
+        
+
+
+
+
+
+    
+    
+
