c01a4142de82e8d1d26816839f2dd6a87d4eacbd
Ciara <ciaratully0@gmail.com>
2020-12-11 12:33:12 +0000

    Fixing HTML file names

--- defineRoutes/routeCustomerDashboard.py
+++ defineRoutes/routeCustomerDashboard.py
@@ -3,4 +3,4 @@ from flask import render_template
 
 @app.route('/User', methods = ['GET'])
 def UserDashboard(): 
-    return(render_template("CustomerDashboard.html"))
+    return(render_template("CustomerPage.html"))
