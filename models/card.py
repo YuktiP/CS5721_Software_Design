a93501a2dce88b526b42e3ac12dc8c2088a328bd
Dhavan Shah <dhavanshah1995@gmail.com>
2020-12-12 13:04:23 +0000

    Adding the blockRequest field in card.py and changed the Card to CreditCard in my DBController to keep things in sync

--- models/card.py
+++ models/card.py
@@ -1,4 +1,5 @@
 from app import db
+
 class CreditCard(db.Model):
     cardId=db.Column(db.BigInteger,primary_key=True)
     cardNumber=db.Column(db.BigInteger)
@@ -11,8 +12,9 @@ class CreditCard(db.Model):
     cardType=db.Column(db.String(20))
     creditLimit=db.Column(db.Integer)
     interest=db.Column(db.Integer)
+    blockRequest=db.Column(db.Integer)
     
-    def __init__(self,cardNumber=None,pin=None,cardCode=None,expiryDate=None,status=None,cardType=None,creditLimit=None,interest=None):
+    def __init__(self,cardNumber=None,pin=None,cardCode=None,expiryDate=None,status=None,cardType=None,creditLimit=None,interest=None,blockRequest=None):
         self.cardNumber=cardNumber
         self.pin=pin
         self.cardCode=cardCode
@@ -21,9 +23,4 @@ class CreditCard(db.Model):
         self.cardType=cardType
         self.creditLimit = creditLimit
         self.interest = interest
-
-
-    def addCard(self):
-            db.create_all()
-            db.session.add(self)
-            db.session.commit()   
+        self.blockRequest = blockRequest   
