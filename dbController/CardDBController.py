446078aa8128c2ee2b64b0977d91cdc85727a07d
riyajoe <72133557+riyajoe@users.noreply.github.com>
2020-12-12 02:39:48 +0000

    Add files via upload

--- dbController/CardDBController.py
+++ dbController/CardDBController.py
@@ -1,24 +1,11 @@
+from models.card import Creditcard
 from app import db
-from models.Card import Card
-
-class CardDBController():
-
+class cardDBController():
     def __init__(self):
         self = self
 
-    def getBlockRequests(self):
-        self.blockCardList = db.session.query(Card.card_number).filter(Card.block_request == 1 , Card.card_status != 0).all()
-        return self.blockCardList
-    
-    def getUnblockRequests(self):
-        self.unblockCardList = db.session.query(Card.card_number).filter(Card.card_status == 0).all()
-        return self.unblockCardList
-
-    def setCardBlockStatus(self, cardNumber):
-        db.session.query(Card).filter(Card.card_number == cardNumber).update({Card.card_status:0})
+    def addCard(self,Creditcard):
+        db.create_all()
+        db.session.add(Creditcard)
         db.session.commit()
 
-    def setCardUnblockStatus(self, cardNumber):
-        db.session.query(Card).filter(Card.card_number == cardNumber).update({Card.card_status:1})
-        db.session.query(Card).filter(Card.card_number == cardNumber).update({Card.block_request:0})
-        db.session.commit()
