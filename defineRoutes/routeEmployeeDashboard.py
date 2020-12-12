73c80bd650f47f0926ae8ef2c9bfc08b4edc75dc
riyajoe <72133557+riyajoe@users.noreply.github.com>
2020-12-12 15:39:15 +0000

    Add files via upload

--- defineRoutes/routeEmployeeDashboard.py
+++ defineRoutes/routeEmployeeDashboard.py
@@ -8,44 +8,38 @@ from werkzeug.utils import secure_filename
 import csv
 import pandas as pd 
 from flask import render_template,redirect,request,url_for
-
+from models.card import Creditcard
+from models.account import Account
 from models.UserAuthentication import User
 from defineRoutes.onBoard import Onboard
 import datetime
 from datetime import date
-from models.onboard.CustomerOnboardService import CustomerOnboardService 
-from models.onboard.CustomerOnboardBatchService import CustomerOnBoardBatchService
-
-UPLOAD_FOLDER='/Users/yuktipatil/MySpace'
+from passgen import Randpass
+UPLOAD_FOLDER='C:/uploads'
 
 
-@app.route('/EmployeeDashboard', methods=['POST','GET'])
-def EmployeeDashboard():   
+@app.route('/BulkUpload', methods=['POST','GET'])
+def BulkUpload():   
     if request.method=='POST':
         file = request.files['file']
         filename = secure_filename(file.filename)
-        docpath=os.path.join(app.config['UPLOAD_FOLDER'],filename)
+        x=os.path.join(app.config['UPLOAD_FOLDER'],filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        
-        onboard = CustomerOnBoardBatchService()
-        data=onboard.onBoardCustomerBatch(docpath)
+        add=Onboard()
+        data=add.BulkOnboard(x)
         flash('File successfully uploaded')
         return(render_template('Display.html',data=data.result))
     return(render_template('OnBoardCustomers.html'))
 
-@app.route('/singleUpload',methods=['POST','GET'])
-def singleUpload():
+@app.route('/EmployeeDashboard',methods=['POST','GET'])
+def EmployeeDashboard():
     f=DashboardFactory()
     fobj=f.getDashboard("employee")
     data=fobj.GetDashboardData()
-
     if request.method=='POST':
         if request.form.get('onboard'):
-            applicationId = request.args.get('id')
-            onboard=CustomerOnBoardService()
-            application = db.session.query(CustomerApplication).filter(CustomerApplication.id == applicationId).first()
-            
-            status=onboard.onBoardCustomer(application)
-            if(status.isSuccess):
-                flash(status.text)
+            add=Onboard()
+            status=add.singleOnboard(data.applications)
+            if(status=='Sucess'):
+                flash('Onboarded Sucessfully')
     return(render_template(data.template,data=data))
